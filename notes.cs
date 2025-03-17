686dd432109374f8fd6a8a39b0e2bf50b829869f
ConanODoyle <samuel.w.sze@gmail.com>
2025-03-17 09:29:44 -0400

    Add notes file

---
+++ notes.cs
@@ -0,0 +1,61 @@
+/*
+delivery components:
+
+gameplay
+- pick up boxes and deliver to the destination
+- arrival phase/zone drops big pallets of boxes, have to unpack and sort
+- delivery phase have to deliver boxes of the right type as quickly as possible
+- no limit, just as many as you can get in
+- can only place boxes on shelves?
+
+- DEATH STRANDING:
+	- speed based distance
+	- tsp solving
+	- more stuff = more unstable (in bl, slower? more noticable/noise generating)
+	- delivering to distance post = unlocks farther deliveries
+
+- SLENDERMAN
+- ROGUELITE
+
+equipment
+- backpack of crates to increase inventory size
+- caffeine pills - speed boost
+
+
+
+
+
+
+survival components:
+
+gameplay
+- deliver boxes to a distant destination
+- a little desert bus, a little infinite mining, a little zombie apoc survival
+- random shops?
+- lots of passive mobs not yet aggroed idling around, patchy spawn algo?
+- time limit for a bonus
+- spend money on equipment prior to leaving
+
+equipment
+- hard hats - more hp
+- high vis vest - cheaper prices at random shops
+- caffeine pills - speed boost
+- aspirin - hp boost (shield)
+- first aid kit
+- duct tape - fix items
+
+weapons (with durability)
+- melee (in approximately ascending tier)
+	- 2x4
+	- bat
+	- knife (short range)
+	- wrench
+	- crowbar
+	- knife pole (long range)
+- ranged - aebase guns? adventure pack?
+	- pistol
+	- smg
+	- semiauto rifle
+	- auto rifle
+	- pump shotty
+*/
